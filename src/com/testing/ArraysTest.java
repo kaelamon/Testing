8e0e36052d42ce6df1b728a63501a8b5d0e8ae21
kaelamon <kaelamon20@gmail.com>
2022-03-19 19:06:41 +0300

    Мой первый тест

---
+++ src/com/testing/ArraysTest.java
@@ -0,0 +1,28 @@
+package com.testing;
+
+import java.util.*;
+
+public class ArraysTest {
+    public static void main(String[] args) {
+        int[] arr1 = {1, 2, 3}; //Инициализируем массив arr1 с числами 1 2 3
+        int[] arr2 = {1, 2, 3}; //Инициализируем массив arr1 с числами 1 2 3
+        int[] arr3 = {1, 2, 3}; //Инициализируем массив arr1 с числами 1 2 3
+        Arrays.fill(arr3, 0, 3, 4); //Заполняем массив с 0 по 3 индекс числом 4
+        System.out.println(Arrays.equals(arr1, arr2)); //Сравниваем массив arr1 и arr2
+        System.out.println(Arrays.equals(arr1, arr3)); //Сравниваем массив arr1 и arr3
+        System.out.println(Arrays.toString(arr3)); //Выводим что у нас там в массиве arr3
+
+        int i1 = 13_37; // Инициируем int с подчеркиванием в середине
+        int i2 = 13____________37; // Инициируем int с большим подчеркиванием в середине
+        float f1 = 13_37; // Инициируем float с подчеркиванием в середине
+        float f2 = 1_3_3_7; // Инициируем float с множественным подчеркиванием
+        double d1 = 13_3.7; // Инициируем double с подчеркиванием в середине
+        double d2 = 1.3_______________3_7; // Инициируем float с множественным подчеркиванием и огромным поддчеркиванием в середине
+        System.out.println(i1); // Ниже вывод результата в консоль
+        System.out.println(i2); // Для того чтобы все сломать достаточно поставить подчеркивание до или после числа
+        System.out.println(f1);
+        System.out.println(f2);
+        System.out.println(d1);
+        System.out.println(d2);
+    }
+}
